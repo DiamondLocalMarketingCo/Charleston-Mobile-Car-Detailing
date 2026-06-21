1254e75f256c8774fb4a56dc6672b64ee016dece
DiamondLocalMarketingCo <mdarwish0523@gmail.com>
2026-06-21 12:16:19 -0400

    Add manual Google reviews

---
+++ src/lib/manualReviews.ts
@@ -0,0 +1,23 @@
+export const manualReviews = [
+  {
+    name: "Daniel Neel",
+    rating: 5,
+    relativeTime: "11 months ago",
+    text:
+      "I Recommend this service. The detailer traveled to my house which was super convenient. They used all their own equipment with the exception of a power outlet and a water hookup. They took their time and left my Grand Cherokee's interior spotless. Will likely use again. Also, prices were very reasonable for the market.",
+  },
+  {
+    name: "Joe Solomon",
+    rating: 5,
+    relativeTime: "11 months ago",
+    text:
+      "It was a breeze to work with Charleston Mobile Car Detailing. This young man picked up my car and gave it the royal treatment, then returned it, night to day. The seats no longer look like their dad's pup had a mud party or that the car is used to carry all kinds of things all year round. I feel A LOT prouder driving it around. I personally think this kind of work is magic. CMCD's work was affordable and communication was quick and easy. Five stars for sure. You can confidently book this summer!",
+  },
+  {
+    name: "Raghad Darwish",
+    rating: 5,
+    relativeTime: "a year ago",
+    text:
+      "Very fast response! I have a two year-old baby and my car was a total mess, my car looked brand new and I was able to book in the same day! Would definitely recommend and I’ll be using the service again",
+  },
+] as const;
